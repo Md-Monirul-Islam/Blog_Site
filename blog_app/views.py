fe95e43754dccb651fc4fafbf9194385efb67164
Md-Monirul-Islam <md.monirulmunna.cse@gmail.com>
2024-03-02 10:47:04 +0600

    crud operation complete

--- blog_app/views.py
+++ blog_app/views.py
@@ -3,6 +3,8 @@ from blog_app.models import Blog
 from .serializers import BlogSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+
 
 # Create your views here.
 
@@ -11,17 +13,30 @@ def blog_list(request):
     if request.method == 'GET':
         all_blogs = Blog.objects.filter(is_public=True)
         serializer = BlogSerializer(all_blogs,many=True)
-        return Response(serializer.data)
+        return Response(serializer.data,status=status.HTTP_200_OK)
     if request.method == 'POST':
         serializer = BlogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=400)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
+@api_view(['GET','PUT','DELETE'])
 def blog_details(request,pk):
-    blog = Blog.objects.get(is_public=True,pk=pk)
-    serializer = BlogSerializer(blog)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        blog = Blog.objects.get(pk=pk)
+        serializer = BlogSerializer(blog)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    if request.method == 'PUT':
+        blog = Blog.objects.get(pk=pk)
+        serializer = BlogSerializer(blog,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        blog = Blog.objects.get(pk=pk)
+        blog.delete()
+        return Response(status=status.HTTP_200_OK)
